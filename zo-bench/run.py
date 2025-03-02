45d9410e13e339592ddf7be16d4217deaabef07e
ezraheching <ezraheching@gmail.com>
2025-03-03 01:08:45 +0200

    Update run.py
    changed line 242 for reason beyond my comprehension

--- zo-bench/run.py
+++ zo-bench/run.py
@@ -239,7 +239,7 @@ class Framework:
                     torch_dtype = torch.float16
                 elif self.args.load_bfloat16:
                     torch_dtype = torch.bfloat16
-                model = AutoModelForCausalLM.from_pretrained(self.args.model_name, config=config, device_map='auto',
+                model = AutoModelForCausalLM.from_pretrained(self.args.model_name, config=config, device_map='auto', offload_folder="/tmp/offload",
                                                              torch_dtype=torch_dtype,
                                                              max_memory={i: f'{free_in_GB - 5}GB' for i in
                                                                          range(torch.cuda.device_count())},
